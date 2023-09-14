4dc25324415c9e1073ded92600ded789c6a25bb1
CaioKWY <caiokaway@gmail.com>
2023-09-13 22:37:08 -0300

    atividades curso

---
+++ TesteTipos/src/testetipos/TesteTipos.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testetipos;
+
+/**
+ *
+ * @author caio
+ */
+public class TesteTipos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int idade = 30;
+        String valor = Integer.toString(idade);
+        
+        int idadeStringAgain = Integer.parseInt(valor);
+        
+        System.out.println(idadeStringAgain);
+        System.out.println(valor);
+    }
+    
+    
+}
